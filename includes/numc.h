f4b09738761fa69bcd453be6c2173f1096e64077
fabiocfabini <fabiolucas.carneiro@gmail.com>
2022-10-11 21:13:39 +0100

    Added numc.h include file

---
+++ includes/numc.h
@@ -0,0 +1,16 @@
+#ifndef __NUMC_H__
+#define __NUMC_H__
+
+// Definitions
+// Macros
+// Enums
+// Structs
+// functions
+
+#ifdef NUMC_IMPLEMENTATION
+// This is the implementation of the library. It is only included if
+// NUMC_IMPLEMENTATION is defined before including the header.
+
+#endif // NUMC_IMPLEMENTATION
+
+#endif  // __NUMC_H__
