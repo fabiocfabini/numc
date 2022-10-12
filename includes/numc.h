6cbb31181f31fe815ab0b66e1317164af45c47be
fabiocfabini <fabiolucas.carneiro@gmail.com>
2022-10-12 19:16:07 +0100

    Implemented typechecking macro

--- includes/numc.h
+++ includes/numc.h
@@ -5,8 +5,14 @@
 #define NCDEF
 #endif // NCDEF
 
+// includes
+#include <string.h>
+
 // Definitions 
 // Macros
+#define typeof(expr) (_Generic((expr), char: "char", char*: "char*", unsigned char: "unsigned char", signed char: "signed char",short: "short", unsigned short: "unsigned short",int: "int", int*: "int*", unsigned int: "unsigned int",long: "long", unsigned long: "unsigned long",long long: "long long", unsigned long long: "unsigned long long",float: "float",double: "double",long double: "long double",void*: "void*", default: "?")) 
+#define ISA(expr, type) (strcmp(typeof(expr), type) == 0)
+
 // Enums
 // Structs
 // functions
