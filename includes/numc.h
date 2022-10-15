19a61bb362d820c928fc45bc66dca4d3075011c5
fabiocfabini <fabiolucas.carneiro@gmail.com>
2022-10-15 02:06:41 +0100

    Updated header to fit previous changes

--- includes/numc.h
+++ includes/numc.h
@@ -9,6 +9,8 @@
 
 
 #define MAX_TYPE_LEN 5
+#define NC_TABLE_CAP 10
+#define FFMT(col, precision) "%-" #col "." #precision "f"
 
 #define typeof(expr) (_Generic((expr), char: "char", char*: "char*", unsigned char: "unsigned char", signed char: "signed char",short: "short", unsigned short: "unsigned short",int: "int", int*: "int*", unsigned int: "unsigned int",long: "long", unsigned long: "unsigned long",long long: "long long", unsigned long long: "unsigned long long",float: "float",double: "double",long double: "long double",void*: "void*", default: "?")) 
 #define ISA(expr, type) (strcmp(typeof(expr), type) == 0)
@@ -17,10 +19,13 @@
 typedef struct nc_array{
     int length;
     void* data;
-    short int ndim;
-    short int* shape;
+    int ndim;
+    int* shape;
     char type[MAX_TYPE_LEN];
-} *NCarray, **NCtable;
+} *NCarray;
+
+extern size_t nc_count;
+extern NCarray NC_TABLE[NC_TABLE_CAP];
 
 /**
  * @brief Frees the memory allocated for an NCarray
@@ -30,11 +35,35 @@ typedef struct nc_array{
 void nc_free_array(NCarray arr);
 
 /**
+ * @brief Frees the memory allocated for all NCarrays
+ *      Calling this function is optional, as the memory will be freed
+ *     when the program exits.
+ *
+ */
+void nc_collect();
+
+/**
  * @brief Initializes an NCarray with default values
  * 
  * @return NCarray The initialized NCarray
  */
 NCarray nc_init_array();
+
+/**
+ * @brief Compares two NCarrays
+ * 
+ * @param arr1 The first NCarray
+ * @param arr2 The second NCarray
+ * @return return enum from NC_CMP_STATUS 
+ */
+int nc_equal(NCarray arr1, NCarray arr2);
+
+/**
+ * @brief Prints an NCarray to stdout
+ * 
+ * @param arr The NCarray to print
+ */
+void nc_show(NCarray arr);
 
 /**
  * @brief Creates an NCarray with all values set to 0
@@ -44,7 +73,7 @@ NCarray nc_init_array();
  * @param ... The size of each dimension. Must be ndim arguments
  * @return NCarray The initialized NCarray
  */
-NCarray nc_zeros(const char* type, short int ndim, ...);
+NCarray nc_zeros(const char* type, int ndim, ...);
 
 /**
  * @brief Creates an NCarray with all values set to 1
@@ -54,7 +83,7 @@ NCarray nc_zeros(const char* type, short int ndim, ...);
  * @param ... The size of each dimension. Must be ndim arguments
  * @return NCarray The initialized NCarray
  */
-NCarray nc_ones(const char* type, short int ndim, ...);
+NCarray nc_ones(const char* type, int ndim, ...);
 //TODO: Implement nc_fill
 //TODO: Implement printing of arrays (could be a Depth First Search)
 
@@ -79,24 +108,24 @@ NCarray nc_ones(const char* type, short int ndim, ...);
 // - the size of the array (int)
 // - the shape of the array (which is a pointer to an array of ints)
 //NOTE: Talk to Miguel about implementation
-struct numc_array {
-    void* array;
-    char* type;
-    int size;
-    int* shape;
-};
+// struct numc_array {
+//     void* array;
+//     char* type;
+//     int size;
+//     int* shape;
+// };
 
-// A function that creates a new NCarray struct
-// It takes a pointer to an array, the type of the array, the size of the array, and the shape of the array
-// It returns a pointer to the new NCarray struct
-struct numc_array* new_numc_array(void* array, char* type, int size, int* shape) {
-    struct numc_array* new_array = malloc(sizeof(struct numc_array));
-    new_array->array = array;
-    new_array->type = type;
-    new_array->size = size;
-    new_array->shape = shape;
-    return new_array;
-}
+// // A function that creates a new NCarray struct
+// // It takes a pointer to an array, the type of the array, the size of the array, and the shape of the array
+// // It returns a pointer to the new NCarray struct
+// struct numc_array* new_numc_array(void* array, char* type, int size, int* shape) {
+//     struct numc_array* new_array = malloc(sizeof(struct numc_array));
+//     new_array->array = array;
+//     new_array->type = type;
+//     new_array->size = size;
+//     new_array->shape = shape;
+//     return new_array;
+// }
 
 
 
--- src/main.c
+++ src/main.c
@@ -3,33 +3,12 @@
 
 #include "../includes/numc.h"
 
-
 int main() {
-    NCarray arr = nc_zeros("i32", 2, 2, 2);
+    NCarray arr1 = nc_zeros("i32", 2, 2, 2);
+    NCarray arr2 = nc_zeros("i32", 2, 2, 2);
 
-    // Add 1 to all the values
-    for(int i = 0; i < arr->length; i++){
-        ((int*)arr->data)[i] += 1;
-    }
+    nc_equal(arr1, arr2);
 
-    // Print the values
-    for(int i = 0; i < arr->length; i++){
-        printf("%d ", ((int*)arr->data)[i]);
-    }
-
-    NCarray arr2 = nc_ones("f32", 2, 2, 2);
-
-    // Add i to all the values
-    for(int i = 0; i < arr2->length; i++){
-        ((float*)arr2->data)[i] += 1;
-    }printf("\n");
-
-    // Print the values
-    for(int i = 0; i < arr2->length; i++){
-        printf("%.2f ", ((float*)arr2->data)[i]);
-    }printf("\n");
-
-    nc_free_array(arr);
-    nc_free_array(arr2);
+    nc_collect();
     return 0;
 }
